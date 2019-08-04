1edadc871d6cecf9fe501aadd60dadd0c40b1b54
Ping Chen <remotenonsense@gmail.com>
2019-08-04 10:22:16 +0900

    No need to export ffi functions

--- src/SVGO.purs
+++ src/SVGO.purs
@@ -1,4 +1,8 @@
-module SVGO where
+module SVGO
+  ( SVGO
+  , newSvgo
+  , optimize
+  ) where
 
 import Prelude
 
@@ -10,10 +14,12 @@ import Foreign (Foreign)
 
 data SVGO
 
+-- | Create a SVGO instance from a config object.
 newSvgo :: Foreign -> Aff SVGO
 newSvgo = liftEffect <<< runEffectFn1 svgo_
 foreign import svgo_ :: EffectFn1 Foreign SVGO
 
+-- | Pass svg source to SVGO, get the optimized output.
 optimize :: String -> SVGO -> Aff String
 optimize = (compose toAffE) <<< runEffectFn2 optimize_
 foreign import optimize_ :: EffectFn2 String SVGO (Promise String)
